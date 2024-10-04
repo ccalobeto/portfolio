5139afd64f7601c96a26be3a51006b016e114df7
ccalobeto <calobeto.leon@gmail.com>
2024-10-03 19:59:01 -0500

    update dependencies to optimized images

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,18 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import path from 'path';
+import { defineConfig } from 'vite';
+import { enhancedImages } from '@sveltejs/enhanced-img';
 
-/** @type {import('vite').UserConfig} */
-const config = {
-	plugins: [sveltekit()],
+// /** @type {import('vite').UserConfig} */
+export default defineConfig({
+	plugins: [
+		enhancedImages(),
+		sveltekit()],
 	resolve: {
 		alias: {
 			$routes: path.resolve('./src/routes')
 		}
 	}
-};
+});
 
-export default config;
+// export default config;
